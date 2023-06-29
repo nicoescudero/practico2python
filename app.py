b3a789fff5725db560297f360cb4419c21213909
nicoescudero <nicolas.escudero00@gmail.com>
2023-06-29 08:53:39 -0300

    update: styles

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-import pymysql
-from sqlalchemy import exc
 from flask import Flask,request,render_template,redirect
 from dotenv import load_dotenv
 
@@ -13,10 +11,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 from models import db,Asistencia,Curso,Estudiante,Padre,Preceptor
 
 @app.route('/',methods = ['GET'])
-def index(): return render_template('index.html')
+def index(): return render_template('index.html',message='')
 
 @app.route('/home', methods = ['GET'])
-def home():return render_template('main.html')
+def home():return render_template('main.html',message='')
 
 @app.route('/login',methods = ['POST'])
 def login():
@@ -24,11 +22,13 @@ def login():
 		correo = request.form.get('correo')
 		clave = request.form.get('clave')
 		user = Preceptor.query.filter_by(correo=correo).first()
-		if (user == None): return 'Not Found',404
+		if (user == None):
+			return render_template('index.html',message = 'Usuario no encontrado')
 		if (user.clave == clave):
 			return redirect('/home')
+		return render_template('index.html',message = 'Contraseña incorrecta')
 	except:
-		return render_template('error.html')
+		return render_template('index.html',message = 'Email o Contraseña Incorrectos')
 
 @app.route('/asistencia', methods = ['POST'])
 def registar():
@@ -38,23 +38,23 @@ def registar():
 		asistio = request.form.get('asistio')
 		justificacion = request.form.get('justificacion')
 		estudianteID = request.form.get('estudianteID')
-		print(fecha)
 		data = Asistencia(fecha,codigo,estudianteID,asistio,justificacion)
 		db.session.add(data)
 		db.session.commit()
-		return render_template('success.html')
+		return render_template('main.html',message='Asistencia registrada')
 	except:
-		return 'error',404
+		return render_template('error.html')
 	
 @app.route('/listado', methods = ['GET'])
 def listado():
 	try:
 		data = Asistencia.query.all()
-		print(data)
 		return render_template('listado.html',inasistencias = data)
 	except:
-		return 'SOME ERROR',404
+		return render_template('error.html')
+
 with app.app_context():
 	    db.create_all()
+
 if __name__ == '__main__':
 	app.run(debug = True)
--- models.py
+++ models.py
@@ -4,51 +4,55 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
 class Preceptor(db.Model):
-    __tablename__= 'preceptor'
+    __tablename__= 'Preceptor'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30),nullable=False)
     apellido = db.Column(db.String(30),nullable=False)
     correo = db.Column(db.String(40), unique = True,nullable=False)
     clave = db.Column(db.String(150),nullable=False)
+    curso = db.relationship('Curso', backref='Preceptor', cascade="all, delete-orphan")
     def __init__(self,nombre='',apellido='',correo='',clave=''):
         self.nombre = nombre
         self.apellido = apellido
         self.correo = correo
         self.clave = clave
 
+class Curso(db.Model):
+    __tablename__= 'Curso'
+    id = db.Column(db.Integer, primary_key=True)
+    año = db.Column(db.Integer,nullable=False)
+    division = db.Column(db.Integer,nullable=False)
+    preceptor_id = db.Column(db.Integer, db.ForeignKey('Preceptor.id'),nullable=False)
+    estudiante = db.relationship('Estudiante', backref='Curso', cascade="all, delete-orphan")
+
+class Estudiante(db.Model):
+    __tablename__= 'Estudiante'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30),nullable=False)
+    apellido = db.Column(db.String(30),nullable=False)
+    dni = db.Column(db.String(40), unique = True,nullable=False)
+    curso_id = db.Column(db.Integer, db.ForeignKey('Curso.id'),nullable=False)
+    padre = db.relationship('Padre', backref='Estudiante', cascade="all, delete-orphan")
+    asistencia = db.relationship('Asistencia', backref='Estudiante', cascade="all, delete-orphan")
+
 class Padre(db.Model):
-    __tablename__= 'padre'
+    __tablename__= 'Padre'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30))
     apellido = db.Column(db.String(30))
     correo = db.Column(db.String(40), unique = True)
     clave = db.Column(db.String(150))
-    estudiante_id = db.Column(db.Integer, db.ForeignKey('estudiante.id'),nullable=False)
-
-class Estudiante(db.Model):
-    __tablename__= 'estudiante'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(30),nullable=False)
-    apellido = db.Column(db.String(30),nullable=False)
-    dni = db.Column(db.String(40), unique = True,nullable=False)
-    curso_id = db.Column(db.Integer, db.ForeignKey('curso.id'),nullable=False)
-
-class Curso(db.Model):
-    __tablename__= 'curso'
-    id = db.Column(db.Integer, primary_key=True)
-    año = db.Column(db.Integer,nullable=False)
-    division = db.Column(db.Integer,nullable=False)
-    preceptor_id = db.Column(db.Integer, db.ForeignKey('preceptor.id'),nullable=False)
+    estudiante_id = db.Column(db.Integer, db.ForeignKey('Estudiante.id'),nullable=False)
 
 
 class Asistencia(db.Model):
-    __tablename__= 'asistencia'
+    __tablename__= 'Asistencia'
     id = db.Column(db.Integer, primary_key=True)
     fecha = db.Column(db.DateTime,nullable=False)
     codigoClase = db.Column(db.Integer,nullable=False)
     asistio = db.Column(db.String(10), default = 'Si')
     justificacion = db.Column(db.String(30),nullable=True)
-    estudiante_id = db.Column(db.Integer, db.ForeignKey('estudiante.id'),nullable=False)
+    estudiante_id = db.Column(db.Integer, db.ForeignKey('Estudiante.id'),nullable=False)
     def __init__(self,fecha='',codigoClase='',estudiante_id='',asistio='si',justificacion=''):
         self.fecha=fecha
         self.codigoClase=codigoClase
